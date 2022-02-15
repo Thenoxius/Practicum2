707480888763400ab702cad3704d8d6ab1b6574b
Thenoxius <thomasvanrens@gmail.com>
2022-02-15 15:59:11 +0100

    voor de practica

---
+++ src/week1/practiva2/Zwembad.java
@@ -0,0 +1,26 @@
+package week1.practiva2;
+
+public class Zwembad {
+    private double lengte;
+    private double breedte;
+    private double diepte;
+    public Zwembad (double lg, double br, double dp){
+        lengte = lg;
+        breedte = br;
+        diepte = dp;
+    }
+    public Zwembad(){}
+    public double getLengte(){return lengte;}
+    public double getBreedte(){return  breedte;}
+    public double getDiepte(){return diepte;}
+    public void setLengte(double nwLg){lengte = nwLg;}
+    public void setBreedte(double nwBr){breedte = nwBr;}
+    public void setDiepte(double nwDp){diepte = nwDp;}
+    public double inhoud(){return lengte * breedte * diepte;}
+    public String toString() {
+        String s ="Dit zwembad is "+ breedte +" meter breed, "+lengte+" meter lang, en "+diepte+" meter diep";
+        return s;}
+
+
+
+}
